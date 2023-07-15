701acc4cc6ce4812618fa23447b75accd10c2dee
doraluz <doraluz.gzatarain@gmail.com>
2023-07-15 15:00:33 -0600

    add new project

---
+++ portalweb/src/main/java/com/fotocondeproducciones/portalweb/PortalwebApplication.java
@@ -0,0 +1,13 @@
+package com.fotocondeproducciones.portalweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalwebApplication.class, args);
+	}
+
+}
